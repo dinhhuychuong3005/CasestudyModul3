960b4df134487a6a7c4f50e7905820201ca003a4
lamhiep2108 <hiepnothip@gmail.com>
2021-07-06 11:23:30 +0700

    'hiep'

--- src/main/java/controller/ProductServlet.java
+++ src/main/java/controller/ProductServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+sdadad
     }
 
     @Override
